6e4574ec5b2fa595a3afec20aee6b6b4131d8e95
yveso <yveso@users.noreply.github.com>
2020-04-03 15:28:11 +0200

    Some first styling

--- src/primitives/AppDiv.js
+++ src/primitives/AppDiv.js
@@ -1,7 +1,12 @@
 import styled from "styled-components";
 
 const MainDiv = styled.div`
+  height: 100vh;
+  width: 100vw;
+  margin: 0;
   font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 20px;
+  color: antiquewhite;
 `;
 
 export default MainDiv;
