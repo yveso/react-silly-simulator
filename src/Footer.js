a14fd3d2d08cc4ea29e0e9e82c5fd9398f86b6da
yveso <yveso@users.noreply.github.com>
2020-04-12 23:48:31 +0200

    Some more styling

---
+++ src/Footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      Obacht:
+      <ul>
+        <li>
+          <strong>
+            Diese App enbehrt jeglicher (wissenschaftlicher) Grundlage
+          </strong>
+        </li>
+        <li>
+          Das einzig Sinnvolle an dieser App ist, dass sie exponentielles
+          Wachstum visualisieren kann.
+        </li>
+        <li>
+          Solltest Du -wider Erwarten- eine interessante Entdeckung in dieser
+          App machen, dann überprüfe zunächst ob sie sich reproduzieren lässt.
+          Ausreißer oder Anomalien können immer auftauchen.
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Footer;
--- src/primitives/Containers.js
+++ src/primitives/Containers.js
@@ -1,12 +1,19 @@
 import styled from "styled-components";
 
-const MainDiv = styled.div`
+const AppDiv = styled.div`
   height: 100vh;
   width: 100vw;
   margin: 0;
   font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
   font-size: 20px;
   color: antiquewhite;
+  display: flex;
+  flex-direction: column;
 `;
 
-export default MainDiv;
+const MainDiv = styled.div`
+  flex-grow: 2;
+  margin: 20px 0px;
+`;
+
+export { AppDiv, MainDiv };
